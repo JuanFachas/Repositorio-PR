e4175c6c586a6e31a5349d630bafea6baeaa406f
SantiagoForero123 <galindodavidsantiago520@gmail.com>
2025-05-07 16:52:57 -0500

    Moví README.md a la carpeta OTROS

--- OTROS/Piston_Rojo/assets/js/T-Titule.js
+++
@@ -1,7 +0,0 @@
-document.addEventListener("DOMContentLoaded", () => {
-  const titulo = document.getElementById("titulo-principal");
-
-  // Aplicar la animación al título
-  titulo.style.animation =
-    "fadeInOut 5s ease-in-out infinite, colorChange 10s linear infinite";
-});
